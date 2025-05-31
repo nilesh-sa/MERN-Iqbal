8b4596f67630070088e92e78827f6a59d693a4af
Mohammad Iqbal <63330022+Iqbal0786@users.noreply.github.com>
2025-05-31 16:18:42 +0530

    add change password route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser, userLogin, VerifyUserAccount } from '../controller/auth.controller'; // Adjust the import path as necessary
+import { changedPassword, registerUser, userLogin, VerifyUserAccount } from '../controller/auth.controller'; // Adjust the import path as necessary
 import handleValidation from '../middleware/validator/validator';
-import { loginValidation, registerValidation } from '../validations/auth.validations';
+import { loginValidation, passwordUpdateValidation, registerValidation } from '../validations/auth.validations';
 import { uploadSingle } from '../middleware/multer/upload';
+import authenticate from '../middleware/authentication/authenticate';
 const router = express.Router();
 
 router.post(
@@ -25,5 +26,11 @@ router.post(
 "/verifyAccount",
 VerifyUserAccount 
 );
+router.patch(
+  "/updatePassword",
+  handleValidation(passwordUpdateValidation),
+   authenticate,
+  changedPassword
+)
 
 export default router;
