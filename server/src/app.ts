64da89ddb37ed9c1abe8255f50145b6bd0be9340
Mohammad Iqbal <63330022+Iqbal0786@users.noreply.github.com>
2025-05-31 20:07:36 +0530

    update cors header

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //global middlewares
-app.use(cors());
+app.use(cors({
+  origin:"*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve static files from the uploads directory
 
