f38548a32b96596c809c3330efc09a35af0482c7
Mohammad Iqbal <63330022+Iqbal0786@users.noreply.github.com>
2025-05-31 16:18:55 +0530

    update authenticate middleware

--- server/src/middleware/authentication/authenticate.ts
+++ server/src/middleware/authentication/authenticate.ts
@@ -1,57 +1,35 @@
-import dotenv from "dotenv";
-dotenv.config();
-import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
 
-const verifyToken = (token:string) => {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, `${process.env.JWT_SECRET_KEY}`, (err, user) => {
-      if (err) return reject(err);
+import { Response, NextFunction } from "express";
+import { AuthReq } from "../../controller/auth.controller";
+import { verifyJWT } from "../../utils/auth.utils";
 
-      resolve(user);
-    });
-  });
-};
-//!defining type for authenitcate function
-export interface AuthenticatedRequest extends Request {
-    user: any; // Define the type of the 'user' property here
-  }
- const authenticate = ()=>{
-  return async (req:AuthenticatedRequest, res:Response, next:NextFunction) => {
-    // check if authorization header has been set
-    // if not throw an errors
-    
-    if (!req.headers.authorization)
-      return res.status(400).send({
-        message: "authorization token was not provided or was not valid",
+const authenticate = async (req: AuthReq, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(400).json({
+        message: "Authorization token was not provided or was not valid",
       });
-  
-    // if bearer token is in authorization header
-    // if not throw an error
-    if (!req.headers.authorization.startsWith("Bearer "))
-      return res?.status(400).send({
-        message: "authorization token was not provided or was not valid",
-      });
-  
-    // split the bearer token and get the [1] which is the token
-    const token = req.headers.authorization.split(" ")[1];
-  
-    // then we will call jwt to verify the token
-    let user:any;
-    // if token is invalid then we will throw an error
-    try {
-      user = await verifyToken(token);
-    } catch (err) {
-      return res.status(400).send({
-        message: "authorization token was not provided or was not valid",
-      });
+      return;
     }
-  
-    // if token is valid then we will put the user retrieved from the token in the req object
-    req.user = user.user;
-  
-    // return next()
-    return next();
-  };
-}
-export  default authenticate;
+
+    const token = authHeader.split(" ")[1];
+    const decoded =  verifyJWT(token);
+
+    if (!decoded) {
+      res.status(401).json({ message: "Invalid or expired token" });
+      return;
+    }
+
+    req.user = decoded as any; // Adjust type if needed
+    next();
+  } catch (err) {
+    console.error("Auth Middleware Error:", err);
+    res.status(401).json({
+      message: "Authorization token was not provided or was not valid",
+    });
+  }
+};
+
+export default authenticate;
